50bba9063e5660f21d12b9127ad32d7eb8089be7
Jay Hayes <ur@iamvery.com>
2019-12-07 14:16:00 -0600

    Add recursive calculation for part 2

--- ruby/lib/fuel_cost.rb
+++ ruby/lib/fuel_cost.rb
@@ -2,4 +2,10 @@ module FuelCost
   def self.call(mass:)
     (mass / 3.0).floor - 2
   end
+
+  def self.calculate(mass:)
+    cost = call(mass: mass)
+    return 0 if cost.negative?
+    cost + calculate(mass: cost)
+  end
 end
--- ruby/spec/fuel_cost_spec.rb
+++ ruby/spec/fuel_cost_spec.rb
@@ -7,6 +7,10 @@ RSpec.describe FuelCost do
       expect(described_class.(mass: 14)).to eq(2)
       expect(described_class.(mass: 1969)).to eq(654)
       expect(described_class.(mass: 100756)).to eq(33583)
+
+      expect(described_class.calculate(mass: 14)).to eq(2)
+      expect(described_class.calculate(mass: 1969)).to eq(966)
+      expect(described_class.calculate(mass: 100756)).to eq(50346)
     end
   end
 end
