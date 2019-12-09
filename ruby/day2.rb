619824e0412f504d526799561241ba40b4ce67bd
Jay Hayes <ur@iamvery.com>
2019-12-09 10:45:19 -0600

    Add solution for day 2, part 2

--- ruby/day2.rb
+++ ruby/day2.rb
@@ -11,3 +11,18 @@ output_memory = Intcode.run(input_memory)
 output_program = output_memory.join(",")
 
 puts "(part 1) Final program state: #{output_program}"
+
+expected_output = 19690720
+
+values = (0..99).to_a
+noun, verb = values.product(values).find do |(noun, verb)|
+  input_memory = codes.dup
+  input_memory[1] = noun
+  input_memory[2] = verb
+  output_memory = Intcode.run(input_memory)
+  output_memory.first == expected_output
+end
+
+answer = 100 * noun + verb
+
+puts "(part 2) 100 * noun + verb = #{answer}"
