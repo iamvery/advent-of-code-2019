ec457757145c0ccb078ad829f981b46073be9321
Jay Hayes <ur@iamvery.com>
2019-12-28 22:39:08 -0600

    Extract method for intersections

--- ruby/lib/wiring.rb
+++ ruby/lib/wiring.rb
@@ -8,11 +8,14 @@ class Wiring
   end
 
   def distance
-    intersections = @wire1.points & @wire2.points
     intersections.map { |p| manhattan_distance(Point.new(0,0), p) }.min
   end
 
   private
+
+  def intersections
+    @wire1.points & @wire2.points
+  end
 
   def manhattan_distance(point1, point2)
     (point1.x - point2.x).abs + (point1.y - point2.y).abs
