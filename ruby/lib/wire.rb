88317136dc004ca57547f14158806a719b4fd347
Jay Hayes <ur@iamvery.com>
2019-12-31 07:55:57 -0600

    Refactor point accumulation and reduction rather than mutation

--- ruby/lib/wire.rb
+++ ruby/lib/wire.rb
@@ -4,20 +4,21 @@ Wire = Struct.new(:points) do
   def self.parse(path)
     pieces = path.split(",")
     point = Point.new(0,0)
-    points = []
 
-    pieces.each do |piece|
+    points = pieces.flat_map do |piece|
       direction = piece[0]
       steps = Integer(piece[1..])
-      steps.times do
+      steps.times.reduce([]) do |points, _|
         case direction
-        when "U" then points << point = point.up
-        when "D" then points << point = point.down
-        when "R" then points << point = point.right
-        when "L" then points << point = point.left
+        when "U" then point = point.up
+        when "D" then point = point.down
+        when "R" then point = point.right
+        when "L" then point = point.left
         else
           fail "unknown direction #{direction}"
         end
+
+        points << point
       end
     end
 
