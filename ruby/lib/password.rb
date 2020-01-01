a8970e98f3c89e6a6585088a5d61b24290ccb65f
Jay Hayes <ur@iamvery.com>
2020-01-01 16:58:08 -0600

    Add additional password criteria for day 4, part 2

--- ruby/lib/password.rb
+++ ruby/lib/password.rb
@@ -3,8 +3,8 @@ class Password
     digits = value.digits
     pairs = digits.each_cons(2)
     return false unless digits.count == 6
-    return false unless pairs.any? { |(a,b)| a == b }
     return false unless pairs.all? { |(a,b)| a >= b }
+    return false unless digits.chunk_while { |a,b| a == b }.map(&:count).any? { |c| c == 2 }
     return true
   end
 end
--- ruby/spec/password_spec.rb
+++ ruby/spec/password_spec.rb
@@ -10,14 +10,23 @@ RSpec.describe Password do
       expect(described_class.valid?(123456)).to be(false)
     end
 
+    it "must have at least one pair of adjacent digits that are the same" do
+      expect(described_class.valid?(123334)).to be(false)
+      expect(described_class.valid?(111233)).to be(true)
+      expect(described_class.valid?(111123)).to be(false)
+    end
+
     it "must contain only increasing digits from left the right" do
       expect(described_class.valid?(111110)).to be(false)
     end
 
     it "validates examples" do
-      expect(described_class.valid?(111111)).to be(true)
+      expect(described_class.valid?(111111)).to be(false)
       expect(described_class.valid?(223450)).to be(false)
       expect(described_class.valid?(123789)).to be(false)
+      expect(described_class.valid?(112233)).to be(true)
+      expect(described_class.valid?(123444)).to be(false)
+      expect(described_class.valid?(111122)).to be(true)
     end
   end
 end
