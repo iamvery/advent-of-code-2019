d4fd4ad1ee2531e1905cdd759e8f1a52bdf46976
Jay Hayes <ur@iamvery.com>
2020-01-11 13:14:43 -0600

    Change local variable names to clarify intent of values

--- ruby/lib/intcode.rb
+++ ruby/lib/intcode.rb
@@ -1,27 +1,27 @@
 module Intcode
   def self.call(program)
-    integers = program.split(",").map { |i| Integer(i) }
-    result = run(integers)
+    memory = program.split(",").map { |i| Integer(i) }
+    result = run(memory)
     result.join(",")
   end
 
-  def self.run(codes, position = 0)
-    opcode = codes[position]
-    first_input_position = codes[position+1]
-    second_input_position = codes[position+2]
-    output_position = codes[position+3]
+  def self.run(memory, position = 0)
+    opcode = memory[position]
+    first_input_position = memory[position+1]
+    second_input_position = memory[position+2]
+    output_position = memory[position+3]
 
-    codes[output_position] = case opcode
+    memory[output_position] = case opcode
     when 1
-      codes[first_input_position] + codes[second_input_position]
+      memory[first_input_position] + memory[second_input_position]
     when 2
-      codes[first_input_position] * codes[second_input_position]
+      memory[first_input_position] * memory[second_input_position]
     when 99
-      return codes
+      return memory
     else
       fail "Unknown opcode: #{opcode}"
     end
 
-    run(codes, position + 4)
+    run(memory, position + 4)
   end
 end
