7cacca634c11de19c1e0f5adaefdfc4850ed2bd9
Jay Hayes <ur@iamvery.com>
2020-01-14 08:12:52 -0600

    Move parameter work into cases to prep for non-4 param operations

--- ruby/lib/computer.rb
+++ ruby/lib/computer.rb
@@ -13,27 +13,36 @@ module Computer
     digits = instruction.digits
     opcode = digits.take(2)
     parameter_modes = Array(digits[2..])
-    parameter_memory = memory[position+1..position+3]
-    parameters = parameter_memory.zip(parameter_modes).map { |(mem, mode)|
-      case mode
-      when 1 then Parameters::Immediate.new(mem)
-      else
-        Parameters::Positional.new(mem)
-      end
-    }
-    first_input, second_input, output = parameters
 
-    next_memory = case opcode
+    case opcode
     when [1], [1,0]
-      Operations::Addition.new(first_input, second_input, output).call(memory)
-    when [2,], [2,0]
-      Operations::Multiplication.new(first_input, second_input, output).call(memory)
+      parameter_memory = memory[position+1..position+3]
+      parameters = parameter_memory.zip(parameter_modes).map { |(mem, mode)|
+        case mode
+        when 1 then Parameters::Immediate.new(mem)
+        else
+          Parameters::Positional.new(mem)
+        end
+      }
+      first_input, second_input, output = parameters
+      next_memory = Operations::Addition.new(first_input, second_input, output).call(memory)
+      run(next_memory, position + 4)
+    when [2], [2,0]
+      parameter_memory = memory[position+1..position+3]
+      parameters = parameter_memory.zip(parameter_modes).map { |(mem, mode)|
+        case mode
+        when 1 then Parameters::Immediate.new(mem)
+        else
+          Parameters::Positional.new(mem)
+        end
+      }
+      first_input, second_input, output = parameters
+      next_memory = Operations::Multiplication.new(first_input, second_input, output).call(memory)
+      run(next_memory, position + 4)
     when [9,9]
       return memory
     else
       fail "Unknown opcode: #{opcode}"
     end
-
-    run(next_memory, position + 4)
   end
 end
