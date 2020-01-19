cae0605dcebee3971691ef71d9c356c8ce818a14
Jay Hayes <ur@iamvery.com>
2020-01-19 12:35:56 -0600

    Update day 6, part 1 solution to work with latest Orbits abstraction

--- ruby/day6.rb
+++ ruby/day6.rb
@@ -3,7 +3,6 @@ require "space/orbits"
 
 orbit_data = File.read("../input/day6.txt")
 
-bodies = Space::Orbits.process(orbit_data)
-total_orbits = bodies.values.map(&:indirect_orbits).reduce(&:+)
+orbits = Space::Orbits.process(orbit_data)
 
-puts "Total number of orbits (part 1): #{total_orbits}"
+puts "Total number of orbits (part 1): #{orbits.indirect_orbits}"
