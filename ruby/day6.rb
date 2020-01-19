b10ef463798f4f857ffde5c973f9b8d408d70ae1
Jay Hayes <ur@iamvery.com>
2020-01-19 12:36:14 -0600

    Add solution to day 6, part 2

--- ruby/day6.rb
+++ ruby/day6.rb
@@ -6,3 +6,4 @@ orbit_data = File.read("../input/day6.txt")
 orbits = Space::Orbits.process(orbit_data)
 
 puts "Total number of orbits (part 1): #{orbits.indirect_orbits}"
+puts "Total number of orbital transfers (part 2): #{orbits.minimum_transfers("YOU", "SAN")}"
