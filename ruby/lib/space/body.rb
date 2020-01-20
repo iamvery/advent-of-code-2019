059ab0b81874b9394a7d2e12af394e0bfaa7d336
Jay Hayes <ur@iamvery.com>
2020-01-20 08:45:26 -0600

    Simplify indirect_orbits in terms of self to delete Bodies abstraction

--- ruby/lib/space/body.rb
+++ ruby/lib/space/body.rb
@@ -1,7 +1,7 @@
 module Space
   Body = Struct.new(:id, :body) do
     def indirect_orbits
-      bodies.reduce(0) { |s| s+1 }
+      reduce(0) { |s| s+1 } - 1
     end
 
     def to_a
@@ -11,26 +11,6 @@ module Space
     def each(&b)
       yield(self)
       body.each(&b) if body
-    end
-
-    class Bodies
-      include Enumerable
-      attr_reader :body
-
-      def initialize(body)
-        @body = body
-      end
-
-      def each(&b)
-        if body
-          yield(body)
-          body.bodies.each(&b)
-        end
-      end
-    end
-
-    def bodies
-      Bodies.new(body)
     end
   end
 end
