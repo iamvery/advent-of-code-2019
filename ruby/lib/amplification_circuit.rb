2cee8f8fa522795b43e1180eb4ca1eaf5152ec1e
Jay Hayes <ur@iamvery.com>
2020-01-25 14:27:58 -0600

    Refactor amp using pipes instead of stringio
    Flattened out the fancy looping as well. Might try to bring that back
    later.

--- ruby/lib/amplification_circuit.rb
+++ ruby/lib/amplification_circuit.rb
@@ -10,15 +10,25 @@ module AmplificationCircuit
   end
 
   def self.call(program, phase_setting_sequence)
-    phase_setting_sequence.reduce(0) do |input_signal, phase_setting|
-      input = StringIO.new
-      input.puts(phase_setting)
-      input.puts(input_signal)
-      input.rewind
-      output = StringIO.new
-      Computer.call(program.dup, input, output)
-      Integer(output.string)
-    end
+    first_read, first_write = IO.pipe
+    first_write.puts phase_setting_sequence[0]
+    second_read, second_write = IO.pipe
+    second_write.puts phase_setting_sequence[1]
+    third_read, third_write = IO.pipe
+    third_write.puts phase_setting_sequence[2]
+    fourth_read, fourth_write = IO.pipe
+    fourth_write.puts phase_setting_sequence[3]
+    fifth_read, fifth_write = IO.pipe
+    fifth_write.puts phase_setting_sequence[4]
+
+    first_write.puts 0
+    Computer.call(program.dup, first_read, second_write)
+    Computer.call(program.dup, second_read, third_write)
+    Computer.call(program.dup, third_read, fourth_write)
+    Computer.call(program.dup, fourth_read, fifth_write)
+    Computer.call(program.dup, fifth_read, first_write)
+
+    Integer(first_read.gets)
   end
 end
 
