c74f5977336034c105c26fbe65c2d8cdf9c0b687
Jay Hayes <ur@iamvery.com>
2020-01-25 14:29:59 -0600

    Add day 7, part 2 solution

--- ruby/day7.rb
+++ ruby/day7.rb
@@ -3,5 +3,7 @@ require "amplification_circuit"
 
 input_program = File.read("../input/day7.txt")
 sequence, signal = AmplificationCircuit.max(input_program)
-
 puts "(part 1) Ideal phase setting sequence (#{sequence.inspect}) produces the signal: #{signal}."
+
+sequence, signal = AmplificationCircuit.max(input_program, phase_setting_range: 5..9)
+puts "(part 2) Ideal phase setting sequence (#{sequence.inspect}) produces the signal in feedback loop: #{signal}."
