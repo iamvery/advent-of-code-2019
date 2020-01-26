05bf29e653fa48204be5d2cc6dfed9bdc5cb96a7
Jay Hayes <ur@iamvery.com>
2020-01-26 14:02:43 -0600

    Extract SpaceImageFormat to file

--- ruby/spec/space_image_format_spec.rb
+++ ruby/spec/space_image_format_spec.rb
@@ -1,20 +1,4 @@
-#require "space_image_format"
-
-SpaceImageFormat = Struct.new(:layers) do
-  def self.parse(data, width:, height:)
-    pixels = data.split("").map(&method(:Integer))
-    layers = pixels.each_slice(width * height)
-    new(layers.to_a)
-  end
-
-  def checksum
-    counts = layers.map { |layer|
-      layer.each_with_object(Hash.new(0)) { |p,t| t[p] += 1 }
-    }
-    count = counts.min { |c| c[0] }
-    count[1] * count[2]
-  end
-end
+require "space_image_format"
 
 RSpec.describe SpaceImageFormat do
   describe ".parse" do
