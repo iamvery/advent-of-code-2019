399e882c3d10687ea6b1dc0853d00a0770fcd92c
Jay Hayes <ur@iamvery.com>
2020-01-26 14:50:35 -0600

    Remove unneeded pry

--- ruby/day8.rb
+++ ruby/day8.rb
@@ -1,6 +1,5 @@
 $LOAD_PATH.unshift(File.expand_path("./lib/"))
 require "space/image_format"
-require "pry"
 
 image_data = File.read("../input/day8.txt").chomp
 image = Space::ImageFormat.parse(image_data, width: 25, height: 6)
