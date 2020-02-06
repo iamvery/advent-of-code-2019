0a2351675ea0e587ebff561c2ca6fb061401946c
Jay Hayes <ur@iamvery.com>
2020-02-06 08:54:10 -0600

    Ad day 9, part 2 solution

--- ruby/day9.rb
+++ ruby/day9.rb
@@ -3,5 +3,5 @@ require "computer"
 
 input_program = File.read("../input/day9.txt")
 
-puts "Enter mode ('1' for test mode):"
+puts "Enter mode ('1' for test mode [part 1], '2' for sensor boost mode [part 2]):"
 Computer.(input_program)
