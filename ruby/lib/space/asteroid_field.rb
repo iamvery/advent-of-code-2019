99a544a7700db77a84e62a688a85bb6a7c5f83cc
Jay Hayes <ur@iamvery.com>
2020-02-09 12:34:34 -0600

    Move calculations into Asteroid abstraction

--- ruby/lib/space/asteroid_field.rb
+++ ruby/lib/space/asteroid_field.rb
@@ -1,45 +1,23 @@
 require "point"
 
 module Space
-  Asteroid = Struct.new(:point)
-
-  class AsteroidField
-    def self.parse(data)
-      new data.split.flat_map.with_index { |row, y|
-        row.split("").map.with_index { |s, x|
-          Point.new(x,y) if s == "#"
-        }
-      }.compact
-    end
-
-    attr_reader :asteroids
-
-    def initialize(points)
-      @asteroids = points.map { |p| Asteroid.new(p) }
-    end
-
-    def ideal
-      asteroids.map { |asteroid|
-        other_asteroids = asteroids - [asteroid]
-        total = other_asteroids
-          # gather points into groups with the same slope (on the same line) relative to the current point
-          .group_by { |other| slope(asteroid.point, other.point) }
-          # count the number of "visible" points on each line
-          .map { |_slope, points_on_line|
-            # For any slope, there is at most 2 visible points
-            near_points = points_on_line.sort_by { |p| distance(asteroid.point, p.point) }.take(2)
-            next 1 unless near_points.length > 1
-            a,b = near_points
-            # Make sure that the two nearest points aren't on the "same side" and blocking one another
-            next 1 if between(asteroid.point, a.point, b.point)
-            next 1 if between(asteroid.point, b.point, a.point)
-            next 2
-        }.sum
-        [asteroid.point, total]
-      # sort such that the points with the highest total (most visible points)
-      # is last and grab that one, then grab the first element of the result which
-      # is the point itself
-      }.sort_by(&:last).last
+  Asteroid = Struct.new(:point, :field) do
+    def detections
+      other_asteroids = field.asteroids - [self]
+      other_asteroids
+        # gather points into groups with the same slope (on the same line) relative to the current point
+        .group_by { |other| slope(point, other.point) }
+        # count the number of "visible" points on each line
+        .map { |_slope, points_on_line|
+          # For any slope, there is at most 2 visible points
+          near_points = points_on_line.sort_by { |p| distance(point, p.point) }.take(2)
+          next 1 unless near_points.length > 1
+          a,b = near_points
+          # Make sure that the two nearest points aren't on the "same side" and blocking one another
+          next 1 if between(point, a.point, b.point)
+          next 1 if between(point, b.point, a.point)
+          next 2
+      }.sum
     end
 
     private
@@ -57,6 +35,27 @@ module Space
       # adequate for this test since the grid these points is layed out on is in
       # whole units.
       (distance(a,b) + distance(b,c)).round(10) == distance(a,c).round(10)
+    end
+  end
+
+  class AsteroidField
+    def self.parse(data)
+      new data.split.flat_map.with_index { |row, y|
+        row.split("").map.with_index { |s, x|
+          Point.new(x,y) if s == "#"
+        }
+      }.compact
+    end
+
+    attr_reader :asteroids
+
+    def initialize(points)
+      @asteroids = points.map { |p| Asteroid.new(p, self) }
+    end
+
+    def ideal
+      asteroid = asteroids.sort_by(&:detections).last
+      [asteroid.point, asteroid.detections]
     end
   end
 end
