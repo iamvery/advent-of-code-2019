d0e8c830526d986351676364c2a169a11950d991
Jay Hayes <ur@iamvery.com>
2020-02-12 13:03:03 -0600

    Add day 10, part 2 solution

--- ruby/day10.rb
+++ ruby/day10.rb
@@ -3,7 +3,13 @@ require "space/asteroid_field"
 
 data = File.read("../input/day10.txt")
 
-point, total = Space::AsteroidField.parse(data).ideal
+field = Space::AsteroidField.parse(data)
+point, total = field.ideal
 
 puts "Ideal location for base: #{point}"
 puts "with total visibility (part 1): #{total}"
+
+vaporized = field.vaporize
+asteroid = vaporized[199]
+value = asteroid.point.x * 100 + asteroid.point.y
+puts "Value for winning bet (part 2): #{value}"
