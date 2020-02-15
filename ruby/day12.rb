f746e2b02664cf5c1b3310b89d44a37b05d1233f
Jay Hayes <ur@iamvery.com>
2020-02-15 12:52:05 -0600

    Add day 12, part 2 solution

--- ruby/day12.rb
+++ ruby/day12.rb
@@ -5,5 +5,7 @@ data = File.read("../input/day12.txt")
 
 simulation = Space::Simulation.parse(data)
 simulation.step(1000)
+repetition_steps = Space::Simulation.detect_repetition(data)
 
 puts "Total energy in simulation after 1000 steps (part 1): #{simulation.total_energy}"
+puts "Simulation repeats state after steps (part 2): #{repetition_steps}"
