de0314f3a7b44bc51a80e786450530b24f345993
Jay Hayes <ur@iamvery.com>
2020-02-15 21:41:14 -0600

    Remove redundant run in spec

--- ruby/spec/arcade/cabinet_spec.rb
+++ ruby/spec/arcade/cabinet_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe Aracde::Cabinet do
       program = "104,1,104,2,104,3,104,6,104,5,104,4,99"
       cabinet = described_class.new(program)
 
-      cabinet.run
       expect { cabinet.run }.to output(<<~SCREEN).to_stdout
        ┌───────┐
        │       │
