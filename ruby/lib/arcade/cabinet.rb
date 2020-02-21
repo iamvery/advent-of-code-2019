35d1c0c4da4bbcd21aa882874797d699b7b2fe5f
Jay Hayes <ur@iamvery.com>
2020-02-20 21:26:45 -0600

    Switch cabinet to threaded implementation
    This will allow it to run while taking input and moving the game
    forward.

--- ruby/lib/arcade/cabinet.rb
+++ ruby/lib/arcade/cabinet.rb
@@ -10,28 +10,15 @@ module Aracde
       @screen = Arcade::Screen.new
     end
 
-    def run(stats: false)
+    def run
       r,w = IO.pipe
-      Computer.(program, $stdin, w)
-      pixels = {}
+      Thread.new { Computer.(program, $stdin, w) }
       loop do
-        Timeout.timeout(0.001) do
-          x = Integer(r.gets)
-          y = Integer(r.gets)
-          tile = Integer(r.gets)
-          pixels.store(Point.new(x,y), tile)
-        end
-      end
-    rescue Timeout::Error
-      pixels.each do |point, tile|
+        x = Integer(op_r.gets)
+        y = Integer(op_r.gets)
+        point = Point.new(x,y)
+        tile = Integer(op_r.gets)
         screen.set(point, tile)
-      end
-
-      if stats
-        puts
-        puts "Tile counts:"
-        puts "block tiles: #{pixels.values.count { |t| t == Arcade::Screen::TILES.index(Arcade::Screen::BLOCK) }}"
-        puts
       end
     end
 
