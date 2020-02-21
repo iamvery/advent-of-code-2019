283f9032132d817cb52ae23c11d2ff6b5e41fc33
Jay Hayes <ur@iamvery.com>
2020-02-20 21:34:49 -0600

    Add another note to day 13 script

--- ruby/day13.rb
+++ ruby/day13.rb
@@ -7,3 +7,4 @@ game = File.read("../input/day13.txt")
 Aracde::Cabinet.new(game).run(auto: true)
 
 puts "Note: The number of 'block' tiles displayed (part 1)"
+puts "Note: The final score was observed in the logs by autoplaying the game to completion (part 2)"
